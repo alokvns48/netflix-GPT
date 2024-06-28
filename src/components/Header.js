11397e8a2911f7245f64a0db466db3f096a46e87
Alok Pandey <alokpandey@Aloks-Laptop.local>
2024-06-29 03:41:50 +0530

    updated fevicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO_URL, SUPPORTED_LANGUAGES } from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
-import {changeLanguage} from '../utils/configSlice'
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const nagivate = useNavigate();
@@ -59,28 +59,30 @@ const Header = () => {
   };
 
   const handleLanguageChange = (e) => {
-    dispatch(changeLanguage(e.target.value))
+    dispatch(changeLanguage(e.target.value));
   };
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
       <img src={LOGO_URL} alt="logo" className="w-44 mx-auto md:mx-0" />
       {user && (
         <div className="flex p-2 items-center justify-between">
-          {showGptSearch && <select
-            className="p-2 m-2 bg-gray-900 text-white"
-            onChange={handleLanguageChange}
-          >
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option value={lang.identifier} key={lang.identifier}>
-                {lang.name}
-              </option>
-            ))}
-          </select>}
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option value={lang.identifier} key={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
           <button
             className="py-2 px-4 mx-4 bg-purple-500 text-white rounded-lg"
             onClick={handleGptSearchClick}
           >
-            {showGptSearch ? "Homepage": "GPT Search"}
+            {showGptSearch ? "Homepage" : "GPT Search"}
           </button>
           <img
             className="hidden md:inline-block w-10 h-10 rounded-lg"
